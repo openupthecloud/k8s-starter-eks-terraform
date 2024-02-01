f5b36389f2762aa3c83684d1a57939f80868fde5
Louis Bichard <louis@gitpod.io>
2024-02-01 09:31:11 +0000

    fix: updated microservices

--- microservice-a/main.js
+++ microservice-a/main.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World from Kubernetes via Microservice A Gitpod CDEs');
+    res.send('Hello World from Kubernetes via Microservice A Gitpod CDEs Again');
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+process.on('SIGINT', () => {
+    console.info("Interrupted")
+    process.exit(0)
+})
